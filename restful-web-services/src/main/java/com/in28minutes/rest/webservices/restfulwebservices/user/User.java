81990257a7f7b442228e30d0c91ac1f6f4f11821
“srinivass2020” <srinivass2020@gmail.com>
2020-05-26 12:01:33 -0400

    Spring Validation and Batch Example

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,12 +2,17 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	
 	private Integer id;
 	
+	@Size(min = 2)
 	private String name;
 	
+	@Past
 	private Date birthDate;
 	
 	
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
@@ -20,11 +22,13 @@ public class UserResource {
 	@Autowired
 	private UserDaoService userService;
 	
+	//http://localhost:8080/users
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers(){
 		return userService.findAll();
 	}
 	
+	//http://localhost:8080/users/1
 	@GetMapping(path="/users/{id}")
 	public User retrieveUser( @PathVariable int id) {
 		System.out.println("Hi FindOne");
@@ -36,14 +40,25 @@ public class UserResource {
 		}
 	}
 	
+	//http://localhost:8080/delusers/1
 	@GetMapping(path="/delusers/{id}")
-	public void deleteUser( @PathVariable int id) {
+	public void deleteUser(@Valid @PathVariable int id) {
 		System.out.println("Hi FindOne");
 		User user = userService.deleteById(id);
 		if(null==user) {
 			throw new UserNotFoundException("id - "+id) ; 
 		}
 	}
+	/**
+	 * headers 	: 	Content-Type = application/json
+	 * Post 	:	http://localhost:8080/users
+	 * Body		:	Type = Custom / Raw
+	 *				{
+  						"name": "Sri",
+  						"birthDate": "2000-05-10T03:29:37.460+0000"
+					}
+	 * 
+	 */
 	@SuppressWarnings("rawtypes")
 	@PostMapping("/users")
 	public ResponseEntity createUser(@RequestBody User user) {
