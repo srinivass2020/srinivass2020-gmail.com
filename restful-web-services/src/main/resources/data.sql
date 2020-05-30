c651d21c6bdcc8a94ca1552b6b525d2d0bc7959e
“srinivass2020” <srinivass2020@gmail.com>
2020-05-30 14:43:04 -0400

    SpringRestfulWebServicesComplete

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values (1,sysdate(),'Sri');
+insert into user values (2,sysdate(),'Mad');
+insert into user values (3,sysdate(),'Dhr');
+
+insert into post values(11001,'MyFirstPost',1);
+insert into post values(11002,'MySecondPost',1);
